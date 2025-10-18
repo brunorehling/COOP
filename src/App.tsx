5a13551bf19d22fec251041640d5df5e057abb58
unknown <brunorehling91@gmail.com>
2025-10-18 16:38:46 -0300

    integrating login/register with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import AuthForm from './components/Form'
+import { Routes, Route} from 'react-router-dom'
+
 import './App.css'
 import Land from './Land'
+import { Login } from './Login'
+import { Cadastro } from './Cadastro'
 
 export default function App() {
   return (
     <div className="min-h-screen bg-[#212C42]">
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} /> {/* redireciona raiz */}
-        <Route path="/login" element={<AuthForm tipo="login" />} />
-        <Route path="/cadastro" element={<AuthForm tipo="cadastro" />} />
-        <Route path="/land" element={<Land/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/" element={<Land/>} />
       </Routes>
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/auth': {
+        target: 'url da api no render',
+        changeOrigin: true,
+        secure: true
+      }
+    }
+  }
 })
